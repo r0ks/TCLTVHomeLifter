6d8b7f40324e33243eaab8f31668f692ae824d95
r0ks <r0ks@users.noreply.github.com>
2024-04-21 16:15:54 +0800

    magisk模块

---
+++ app/src/main/java/magisk/TCLTVHomeLifter/service.sh
@@ -0,0 +1,24 @@
+#!/system/bin/sh
+
+MODDIR=${0%/*}
+logFile="${MODDIR}/Service.log"
+echo "" > $logFile
+
+# 写入日志的功能
+log() {
+#    echo "$(date +'%Y-%m-%d %H:%M:%S') - $1" >> $logFile
+}
+
+# 记录开始执行的时间
+log "Starting service script"
+
+# 确保执行权限
+chmod +x ${MODDIR}/tcl_tv_home_lifter.sh
+log "Set executable permission for tcl_tv_home_lifter.sh"
+
+# 执行脚本
+log "Executing tcl_tv_home_lifter.sh"
+${MODDIR}/tcl_tv_home_lifter.sh
+
+# 记录脚本执行结束
+log "tcl_tv_home_lifter.sh execution completed"
---
+++ app/src/main/java/magisk/TCLTVHomeLifter/tcl_tv_home_lifter.sh
@@ -0,0 +1,99 @@
+#!/system/bin/sh
+
+MODDIR=${0%/*}
+logFile="${MODDIR}/TCLTVHomeLifter.log"
+echo "" > $logFile
+
+# 定义日志函数
+log() {
+#    echo "$(date +'%Y-%m-%d %H:%M:%S') - $1" >> $logFile
+}
+
+# 检查桌面是否已启动
+check_desktop_resumed() {
+    dumpsys activity activities | grep mResumedActivity | grep -E "com.tcl.cyberui/.MainActivity|com.tcl.cyberui/com.tcl.cyberui.MainActivity"
+    return $?
+}
+
+# 检查桌面是否已fucus
+check_desktop_current_focus() {
+    dumpsys window windows | grep mCurrentFocus | grep -E "com.tcl.cyberui/.MainActivity|com.tcl.cyberui/com.tcl.cyberui.MainActivity"
+    return $?
+}
+
+# 拒绝cyberui联网
+reject_cyberui_network() {
+    su -c "iptables -A OUTPUT -m owner --uid-owner 1000 -j REJECT" >> $logFile 2>&1
+    su -c "iptables -A INPUT -m owner --uid-owner 1000 -j REJECT" >> $logFile 2>&1
+}
+
+# 启动emotnui
+pull_up_emotnui() {
+    # home键绑定emotnui
+    su -c "cmd package set-home-activity 'com.oversea.aslauncher/.ui.main.MainActivity'" >> $logFile 2>&1
+    # 拉起emotnui
+    su -c "am start -n com.oversea.aslauncher/.ui.main.MainActivity" >> $logFile 2>&1
+}
+
+# 输出dumpsys结果的函数（已停用，保留方便以后使用）
+dumpsys_to_log() {
+#    log "Current Focus:"
+#    dumpsys window windows | grep mCurrentFocus >> $logFile 2>&1
+#    log "Focused App:"
+#    dumpsys window windows | grep mFocusedApp >> $logFile 2>&1
+#    log "Resumed Activity:"
+#    dumpsys activity activities | grep mResumedActivity >> $logFile 2>&1
+#    log "Top Activity in the stack:"
+#    dumpsys activity | grep top-activity >> $logFile 2>&1
+}
+
+i=0
+# 循环检查直到桌面启动
+while true; do
+    i=$((i + 1))
+    if [ $i -gt 30 ]; then
+        log "----------------------MAIN TOO MUCH LOOPS-----------------------"
+        break;
+    fi
+    log "----------------------MAIN START-----------------------"
+    log "Checking if desktop has resumed..."
+    dumpsys_to_log
+    if check_desktop_resumed; then
+        log "Desktop is resumed, performing actions..."
+        dumpsys_to_log
+        reject_cyberui_network
+        ii=0
+        while true; do
+            ii=$((ii + 1))
+            if [ $ii -gt 60 ]; then
+                log "----------------------SUB TOO MUCH LOOPS-----------------------"
+                break;
+            fi
+            log "----------------------SUB START-----------------------"
+            log "Checking if desktop has been focused..."
+            dumpsys_to_log
+            if check_desktop_current_focus; then
+                dumpsys_to_log
+                # 延迟不够就加这个
+                sleep 2.3
+                log "Desktop is focused, performing actions..."
+                pull_up_emotnui
+                dumpsys_to_log
+                break;
+            else
+                log "Desktop not focused, waiting..."
+                dumpsys_to_log
+            fi
+            sleep 0.2
+            log "----------------------SUB END-----------------------"
+        done
+        log "Actions completed and desktop setup is finalized."
+        break
+    else
+        log "Desktop not resumed, waiting..."
+        dumpsys_to_log
+    fi
+    # 等待400ms再检查一次
+    sleep 0.4
+    log "-----------------------MAIN END------------------------"
+done
