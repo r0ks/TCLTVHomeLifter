492b537c0430828b553d2344c8fc4b9227368bc0
r0ks <r0ks@users.noreply.github.com>
2024-04-21 16:32:25 +0800

    magisk模块加入emotnui，增加默认prop

---
+++ app/src/main/java/magisk/TCLTVHomeLifter/customize.sh
@@ -0,0 +1,2 @@
+# set_perm_recursive  <dirname>  <owner> <group> <dirpermission> <filepermission> <contexts> (default: u:object_r:system_file:s0)
+set_perm_recursive  $MODPATH/system/app/EmotnUI  0  0  0755  0644
